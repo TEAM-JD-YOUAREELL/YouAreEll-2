d923f291eb6352d865093f094ac279346c664d61
dayvidtran <dayvidtran@gmail.com>
2023-04-09 18:44:16 -0400

    connection class

--- Client/src/main/java/controllers/ServerController.java
+++ Client/src/main/java/controllers/ServerController.java
@@ -1,30 +1,58 @@
-import spiffyUrlManipulator
+//import spiffyUrlManipulator
+package controllers;
+import models.Id;
+import views.Connection;
 
-public class ServerController() {
+import javax.json.JsonString;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+
+
+
+public class ServerController {
+    URL urlID = new URL("http://zipcode.rocks:8085/ids");
+
+
     private String rootURL = "http://zipcode.rocks:8085";
 
     private ServerController svr = new ServerController();
 
-    private ServerController() {}
+    private ServerController() throws MalformedURLException {}
 
-    public static shared() {
+    public ServerController shared() {
         return svr;
     }
 
     public JsonString idGet() {
         // url -> /ids/
+        //http://zipcode.rocks:8085/ids is where we get the info.
         // send the server a get with url
+        //git from http://zipcode.rocks:8085/ids
         // return json from server
+        Connection con = new Connection(urlID);
+
+        return null;
     }
-    public JsonString idPost(Id) {
+
+
+    public JsonString idPost(Id id) {
         // url -> /ids/
         // create json from Id
         // request
         // reply
         // return json
+        return null;
     }
-    public JsonString idPut(Id) {
+    public JsonString idPut(Id id) {
         // url -> /ids/
+        return null;
     }
 
 
